b4d70d9b8df1f3b89d257ea5aafe989c67466b65
Emma Closca <emmaclosca@gmail.com>
2024-04-10 15:01:16 +0100

    added add+view

--- toDoList/urls.py
+++ toDoList/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .views import Tasks, AddTask, TaskDetail
 
 urlpatterns = [
-    path('', views.home),
+    path('', Tasks.as_view(), name='tasks'),
+    path('addTask/', AddTask.as_view(), name='addTask'),
+    path('tasks/<int:pk>/', TaskDetail.as_view(), name='taskDetail'),
 ]
--- toDoList/views.py
+++ toDoList/views.py
@@ -1,5 +1,26 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Task
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
-def home(request):
-    return HttpResponse('Hi')
+
+class Tasks(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['count'] = context['tasks'].filter(complete=False).count()
+        return context
+
+class AddTask(CreateView):
+    model = Task
+    fields = ['title', 'description', 'complete']
+    success_url = reverse_lazy('tasks')
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'toDoList/task_detail.html'
